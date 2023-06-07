4fa695d7f591b46390f931618c10a3e59215829c
yohanbarbosa <yohanbar30@gmail.com>
2023-06-06 20:14:54 -0500

    sub pag acerda de

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,1 +1,14 @@
-alert("bienvenido gerrero" );
+let valuedisplay = document.querySelectorAll('.numero');
+let intervalo = 5000;
+
+
+valuedisplay.forEach((valuedisplay) =>{
+let starvalue = 0;
+let endvalue = parseInt(valuedisplay.getAttribute("data-val"));
+let duracion = Math.floor(intervalo / endvalue);
+let contador = setInterval(function(){
+starvalue += 1;
+valuedisplay.textContent = starvalue;
+if()
+});
+});
