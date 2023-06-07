72ee040570f3fce1122fecd989455ddbf95822a1
Adrian Quintana <aequintanaa@ufpso.edu.co>
2023-06-07 11:48:40 -0500

    actualitation of the sub pag

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,14 +1,19 @@
-let valuedisplay = document.querySelectorAll('.numero');
-let intervalo = 5000;
+let valuedisplay = document.querySelectorAll(".numero");
+let interval = 1500;
 
 
-valuedisplay.forEach((valuedisplay) =>{
+valuedisplay.forEach((valuedisplay) => {
 let starvalue = 0;
-let endvalue = parseInt(valuedisplay.getAttribute("data-val"));
-let duracion = Math.floor(intervalo / endvalue);
-let contador = setInterval(function(){
-starvalue += 1;
-valuedisplay.textContent = starvalue;
-if()
-});
+let endvalue = parseInt(valuedisplay.getAttribute
+("data-val"));
+let duration = Math.floor(interval / endvalue);
+let counter = setInterval(function(){
+    starvalue += 1;
+    valuedisplay.textContent = starvalue;
+    if(starvalue == endvalue){
+        clearInterval(counter);
+    }
+} , duration);
+
+
 });
